23c037021c9597288ff76e67b8ec71f5f66612bd
midora <1239988498@qq.com>
2020-04-18 19:06:03 +0800

    modified main_sim.py

--- tcpNode.py
+++ tcpNode.py
@@ -15,6 +15,36 @@ requests = []  # request text file name list
 module_path = os.path.dirname(__file__)
 
 receive_path = 'requestGot'
+found_path = 'objectFound'
+
+
+def multi_send(receive_path, found_path):
+    to_send_files = []
+
+    for root, _, files in os.walk(receive_path):
+        for file in files:
+            if file.split('.')[-1] == 'txt':
+                with open(os.path.join(root, file), 'r') as rf:
+                    lines = rf.readlines()
+                for line in lines:
+                    request = line.strip().split(' ')[-1]
+                    obj_filename = request + '.jpg'
+                    to_send_files.append(os.path.join(found_path, obj_filename))
+    num_to_sent = len(to_send_files)
+    num_name = str(num_to_sent) + '.txt'
+    with open(num_name, 'w') as wf:
+        wf.write(str(num_to_sent))
+    sent = server.sendFile(num_name)
+    time.sleep(1)
+    if not sent:
+        print(num_name + ' failed to send.')
+    for file in to_send_files:
+        sent = server.sendFile(file)
+        time.sleep(1)
+        if not sent:
+            print(file + ' failed to send.')
+    print(str(num_to_sent) + ' files have been sent to client.')
+
 
 def callback(data):
     if data.data == 'Human_detect':
@@ -24,31 +54,7 @@ def callback(data):
         sent = server.sendFile(message)
         time.sleep(1)
     elif data.data == 'Send_Object':
-        to_send_files = []
-        found_path = 'objectFound'
-        for root, _, files in os.walk(receive_path):
-            for file in files:
-                if file.split('.')[-1] == 'txt':
-                    with open(os.path.join(root, file), 'r') as rf:
-                        lines = rf.readlines()
-                    for line in lines:
-                        request = line.strip().split(' ')[-1]
-                        obj_filename = request + '.jpg'
-                        to_send_files.append(os.path.join(found_path, obj_filename))
-        num_to_sent = len(to_send_files)
-        num_name = str(num_to_sent) + '.txt'
-        with open(num_name, 'w') as wf:
-            wf.write(str(num_to_sent))
-        sent = server.sendFile(num_name)
-        time.sleep(1)
-        if not sent:
-            print(num_name + ' failed to send.')
-        for file in to_send_files:
-            sent = server.sendFile(file)
-            time.sleep(1)
-            if not sent:
-                print(file + ' failed to send.')
-        print(str(num_to_sent) + ' files have been sent to client.')
+        multi_send(receive_path, found_path)
 
 
 server = SocketServer()
